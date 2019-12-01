81d92ce8c0d5642d7e7795917b2a83488dccf07d
Hewlett Packard <Hewlett Packard@JR95>
2019-12-01 11:05:33 -0600

    Se creo migraciones, modelo y controlador

---
+++ database/migrations/2019_12_01_164754_crear_alu.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CrearAlu extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alumnos', function (Blueprint $table) {
+            $table->bigIncrements('id')->unique();
+            $table->string('nombreA',30);
+            $table->string('grado',15);
+            $table->string('carrera',25);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
